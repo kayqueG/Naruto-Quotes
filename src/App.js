6582c12cd0cf2cd299d5e4902cb10890c02ae8c8
kayque <kayquegps2@gmail.com>
2022-01-05 17:28:59 -0300

    setting background image and global style

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { GlobalStyle } from './components';
+
+function App() {
+  return (
+    <div>
+      <h1>DATTEBAYO</h1>
+    </div>
+  );
+}
+
+export default App;
